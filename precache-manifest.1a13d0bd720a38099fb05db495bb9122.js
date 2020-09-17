91e9b671fd904ae82bd991289d74db34e83e7816
KLiwin <k.liwinski@gmail.com>
2020-09-17 09:10:23 +0200

    Updates

---
+++ precache-manifest.1a13d0bd720a38099fb05db495bb9122.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e0d0d29bbde1712072bd36ae11b8b1",
+    "url": "/QuizApp/index.html"
+  },
+  {
+    "revision": "f55e2821ee1f5d7a5225",
+    "url": "/QuizApp/static/css/main.4792e6a5.chunk.css"
+  },
+  {
+    "revision": "b1357f39e001c573a9e9",
+    "url": "/QuizApp/static/js/2.9801fc83.chunk.js"
+  },
+  {
+    "revision": "f42d151f61898dd9d5ffe0d9c5238b3c",
+    "url": "/QuizApp/static/js/2.9801fc83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f55e2821ee1f5d7a5225",
+    "url": "/QuizApp/static/js/main.aef5f849.chunk.js"
+  },
+  {
+    "revision": "553ecaa52dc12743a190",
+    "url": "/QuizApp/static/js/runtime-main.ca1486b2.js"
+  },
+  {
+    "revision": "5b6cc9a15dbc8d05aced4cff5004e8db",
+    "url": "/QuizApp/static/media/Sanah.5b6cc9a1.jpg"
+  },
+  {
+    "revision": "9533b255fa465d31a2974f476350aac1",
+    "url": "/QuizApp/static/media/aboutMe.9533b255.svg"
+  },
+  {
+    "revision": "94835222e2c69e7d5c63b3a8f20d041b",
+    "url": "/QuizApp/static/media/aktualnosci.94835222.svg"
+  },
+  {
+    "revision": "a69513b07cb7a16cc6820552e4761a2c",
+    "url": "/QuizApp/static/media/quiz.a69513b0.svg"
+  }
+]);
