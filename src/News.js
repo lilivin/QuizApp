62288b2d930153c76e104ffa01954a91fa332f90
KLiwin <k.liwinski@gmail.com>
2020-09-18 08:28:46 +0200

    first commit

--- src/News.js
+++ src/News.js
@@ -7,53 +7,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import MailchimpSubscribe from "react-mailchimp-subscribe"
 
-const CustomForm = ({ status, message, onValidated }) => {
+const MyForm = ({ status, message, onValidated }) => {
     let email, name;
     const submit = () =>
-      email &&
-      name &&
-      email.value.indexOf("@") > -1 &&
-      onValidated({
-        EMAIL: email.value,
-        NAME: name.value
-      });
-  
+        email &&
+        name &&
+        email.value.indexOf("@") > -1 &&
+        onValidated({
+            EMAIL: email.value,
+            NAME: name.value
+        });
     return (
-      <div
-        style={{
-          background: "#efefef",
-          borderRadius: 2,
-          padding: 10,
-          display: "inline-block"
-        }}
-      >
-        {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
-        {status === "error" && (
-          <div
-            style={{ color: "red" }}
-            dangerouslySetInnerHTML={{ __html: message }}
-          />
-        )}
-        {status === "success" && (
-          <div
-            style={{ color: "green" }}
-            dangerouslySetInnerHTML={{ __html: message }}
-          />
-        )}
-        <br />
-        <input
-          style={{ fontSize: "2em", padding: 5 }}
-          ref={node => (email = node)}
-          type="email"
-          placeholder="Your email"
-        />
-        <br />
-        <button style={{ fontSize: "2em", padding: 5 }} onClick={submit}>
-          Submit
-        </button>
-      </div>
+        <form>
+            <input placeholder="chuj" type="email" />
+            <button onClick={submit}>
+                Submit
+            </button>
+        </form>
     );
-  };
+};
 
 function News() {
     const url = "https://gmail.us17.list-manage.com/subscribe/post?u=8f483363674247f434e90221e&amp;id=81a8814874";
@@ -69,11 +41,12 @@ function News() {
             <MailchimpSubscribe
                 url={url}
                 render={({ subscribe, status, message }) => (
-                    <CustomForm
-                    status={status}
-                    message={message}
-                    onValidated={formData => subscribe(formData)}
-                    />
+                <div>
+                    <MyForm onSubmitted={formData => subscribe(formData)} />
+                    {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
+                    {status === "error" && <div style={{ color: "red" }} dangerouslySetInnerHTML={{__html: message}}/>}
+                    {status === "success" && <div style={{ color: "green" }}>Subscribed !</div>}
+                </div>
                 )}
             />
         </div>
