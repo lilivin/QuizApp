7ef0d604868565daaba8fab4f470bb82a335c84e
KLiwin <k.liwinski@gmail.com>
2020-09-18 07:53:53 +0200

    first commit

--- src/News.js
+++ src/News.js
@@ -7,6 +7,54 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import MailchimpSubscribe from "react-mailchimp-subscribe"
 
+const CustomForm = ({ status, message, onValidated }) => {
+    let email, name;
+    const submit = () =>
+      email &&
+      name &&
+      email.value.indexOf("@") > -1 &&
+      onValidated({
+        EMAIL: email.value,
+        NAME: name.value
+      });
+  
+    return (
+      <div
+        style={{
+          background: "#efefef",
+          borderRadius: 2,
+          padding: 10,
+          display: "inline-block"
+        }}
+      >
+        {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
+        {status === "error" && (
+          <div
+            style={{ color: "red" }}
+            dangerouslySetInnerHTML={{ __html: message }}
+          />
+        )}
+        {status === "success" && (
+          <div
+            style={{ color: "green" }}
+            dangerouslySetInnerHTML={{ __html: message }}
+          />
+        )}
+        <br />
+        <input
+          style={{ fontSize: "2em", padding: 5 }}
+          ref={node => (email = node)}
+          type="email"
+          placeholder="Your email"
+        />
+        <br />
+        <button style={{ fontSize: "2em", padding: 5 }} onClick={submit}>
+          Submit
+        </button>
+      </div>
+    );
+  };
+
 function News() {
     const url = "https://gmail.us17.list-manage.com/subscribe/post?u=8f483363674247f434e90221e&amp;id=81a8814874";
     return (
@@ -18,7 +66,16 @@ function News() {
                 <input placeholder="Wpisz adres email..." className="newsletter__input" />
                 <button className="newsletter__button">Zapisz się!</button>
             </div>
-            <MailchimpSubscribe url={url}/>
+            <MailchimpSubscribe
+                url={url}
+                render={({ subscribe, status, message }) => (
+                    <CustomForm
+                    status={status}
+                    message={message}
+                    onValidated={formData => subscribe(formData)}
+                    />
+                )}
+            />
         </div>
     )
 }
